8e26714f097cb2f6d6c02c496fcd391158f44dd4
MagnusCarlsen26 <khushal.sindhav26@gmail.com>
2024-06-10 01:17:40 +0530

    Made basic logic for the game

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState }from 'react';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import Square from './src/Square'
 
 export default function App() {
-  return (
-    <SafeAreaView className="bg-black w-full h-full">
-      <Text className="text-white">HELLO WORLD</Text>
-      <StatusBar style="auto" />
-    </SafeAreaView>
-  );
+
+	const [board,setBoard] = useState([
+		[0,0,0] ,
+		[0,0,0] ,
+		[0,0,0]
+	])
+	const [turn,setTurn] = useState(0)
+	return (
+		<SafeAreaView className="bg-black w-full h-full flex justify-center items-center">
+			<Text className='text-white m-2'>Player {1 + turn}'s turn </Text>
+			{
+				board.map( (value,row) => 
+					<View className="flex-row ">
+						{
+							value.map( (indivalue,column) => 
+								<Square 
+									value={indivalue}
+									key={column}
+									setTurn={setTurn}
+									row = {row}
+									column = {column}
+									setBoard = {setBoard}
+									turn = {turn}
+									value = {board[row][column]}
+								/>
+							)}
+					</View>
+				)
+			}
+		</SafeAreaView>
+	);
 }
